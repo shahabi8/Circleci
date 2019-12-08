b0d15e0885f65b965f12457ff7663fa0aaf026b2
shahabi8 <44169036+shahabi8@users.noreply.github.com>
2019-12-08 00:24:52 -0500

    Update app.py

--- demos/flask-sklearn/app.py
+++ demos/flask-sklearn/app.py
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info(f"Scaling Payload: {payload}")
+    LOG.info("Scaling Payload: {payload}")
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
@@ -56,12 +56,12 @@ def predict():
 
 
     json_payload = request.json
-    LOG.info(f"JSON payload: {json_payload}")
+    LOG.info("JSON payload: {json_payload}")
     inference_payload = pd.DataFrame(json_payload)
     LOG.info("inference payload DataFrame: {inference_payload}")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    LOG.info(f"prediction {prediction}")
+    LOG.info("prediction {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
